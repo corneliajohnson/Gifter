9d7a14595c38e3429cfff500ab636a22c9bc2485
corneliajohnson <corneliaj91@gmail.com>
2021-01-15 15:53:41 -0500

    added login and logout to header

--- Gifter/client/src/components/Header.js
+++ Gifter/client/src/components/Header.js
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserProfileContext } from "../providers/UserProfileProvider";
 
 const Header = () => {
-  // const [user, setUser] = useState({});
-
-  // useEffect(() => {
-  //   fetch(`/api/userprofile/${1}`)
-  //     .then((res) => res.json())
-  //     .then((user) => setUser(user));
-  // }, []);
-
+  const user = JSON.parse(localStorage.getItem("userProfile"));
+  const { logout } = useContext(UserProfileContext);
   return (
     <nav className="navbar navbar-expand navbar-dark bg-info">
       <Link to="/" className="navbar-brand">
         GiFTER
       </Link>
       <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Feed
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/posts/add" className="nav-link">
-            New Post
-          </Link>
-        </li>
-        {/* <li className="nav-item">
-          <Link to={`/user/1`} className="nav-link">
-            {user.name}
-          </Link>
-        </li> */}
+        {user ? (
+          <>
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Feed
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/posts/add" className="nav-link">
+                New Post
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link onClick={logout} to="/login" className="nav-link">
+                LogOut
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              Login
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
